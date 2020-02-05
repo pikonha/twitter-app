96afd4b150b880a49fe93169e673b0016f801834
Lucas Picollo <lucas.picollo@geekhunter.com.br>
2020-02-05 12:36:21 -0300

    routes and nodemon implemented

--- src/app.js
+++ src/app.js
@@ -3,8 +3,54 @@ import express from "express";
 const app = express();
 const PORT = 3333;
 
-app.get("/", (req, res) => {
-  res.send("Hello GeekHunter!!");
+// Login
+app.post("/login", (req, res) => {
+  // Response
+});
+
+// Criar usuário
+app.post("/users", (req, res) => {
+  // Response
+});
+
+// Encontrar usuário
+app.get("/users/:id", (req, res) => {
+  // Response
+});
+
+// Encontrar tweets do usuário
+app.get("/users/:id/tweets", (req, res) => {
+  // Response
+});
+
+// Encontrar tweet específico do usuário
+app.get("/users/:id/tweets/:id", (req, res) => {
+  // Response
+});
+
+// Criar tweets
+app.post("/tweets", (req, res) => {
+  // Response
+});
+
+// Encontrar todos os tweets
+app.get("/tweets", (req, res) => {
+  // Response
+});
+
+// Encontrar tweet específico
+app.get("/tweets/:id", (req, res) => {
+  // Response
+});
+
+// Deletar tweet específico
+app.delete("/tweets/:id", (req, res) => {
+  // Response
+});
+
+// Atualizar tweet específico (like/unlike)
+app.put("/tweets/:id", (req, res) => {
+  // Response
 });
 
 app.listen(PORT, () => {
