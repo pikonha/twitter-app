e868309a7bb877e8de04dc1ec8e9e6f6b0221b56
Lucas Picollo <lucas.picollo@geekhunter.com.br>
2020-05-01 16:49:49 -0300

    server initialized: express, morgan & routes

---
+++ server/src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import morgan from 'morgan';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(morgan('common'));
+app.use(express.json());
+
+app.use(routes);
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
